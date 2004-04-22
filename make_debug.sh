69ce5cbe0adcca21211dd30c69433cd4dee4427e
chaoticcoyote <chaoticcoyote>
2004-04-22 22:14:32 +0000

    Enhanced the debug build script to support different architectures

--- make_debug.sh
+++ make_debug.sh
@@ -1,11 +1,24 @@
 #! /bin/sh
 #
-#   Build a debuggable, profilable version of the encoder
+#   Build debuggable, profilable versions of the command-line encoder and decoder
 #
-g++ -I. -lxparam -g -pg -O3 -march=athlon-xp -ffast-math -o debug_encoder encoder/encmain.cpp libdirac_encoder/*.cpp libdirac_common/*.cpp libdirac_motionest/*.cpp
+#   Chack for argument
 #
-#   Build a debuggable, profilable version of the decoder
+if test x"$1" = "x"; then
+    echo ;
+    echo usage: make_debug.sh architecture ;
+    echo ;
+    echo where architecture is pentium4, opteron, or another valid valud for -march= ;
+    echo ;
+    exit 1 ;
+fi
 #
-g++ -I. -lxparam -g -pg -O3 -march=athlon-xp -ffast-math -o debug_decoder decoder/decmain.cpp libdirac_decoder/*.cpp libdirac_common/*.cpp libdirac_motionest/*.cpp
+#   build encoder
+#
+g++ -I. -lxparam -g -pg -O3 -march=$1 -ffast-math -o debug_encoder encoder/encmain.cpp libdirac_encoder/*.cpp libdirac_common/*.cpp libdirac_motionest/*.cpp
+#
+#   build decoder
+#
+g++ -I. -lxparam -g -pg -O3 -march=$1 -ffast-math -o debug_decoder decoder/decmain.cpp libdirac_decoder/*.cpp libdirac_common/*.cpp libdirac_motionest/*.cpp
 #
 # END
