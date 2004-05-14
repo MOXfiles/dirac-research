2608ef9071a033a86df064ed6bc5e1630fabd346
stuart_hc <stuart_hc>
2004-05-14 10:47:33 +0000

    Rearranged to use "make" to be more maintainable.

--- make_debug.sh
+++ make_debug.sh
@@ -2,23 +2,18 @@
 #
 #   Build debuggable, profilable versions of the command-line encoder and decoder
 #
-#   Chack for argument
+#   Check for argument
 #
 if test x"$1" = "x"; then
-    echo ;
-    echo usage: make_debug.sh architecture ;
-    echo ;
-    echo where architecture is pentium4, opteron, or another valid valud for -march= ;
-    echo ;
-    exit 1 ;
+    echo
+    echo "usage: make_debug.sh architecture-to-target"
+    echo
+    echo "E.g.   make_debug.sh k8        # opteron CPU"
+    echo "       make_debug.sh pentium4"
+    echo "       make_debug.sh athlon-xp"
+    echo
+    echo "For full details on GCC's targets see \"info gcc\" - Invoking GCC"
+    exit 1
 fi
-#
-#   build encoder
-#
-g++ -I. -lxparam -g -pg -O3 -march=$1 -ffast-math -o debug_encoder encoder/encmain.cpp libdirac_encoder/*.cpp libdirac_common/*.cpp libdirac_motionest/*.cpp
-#
-#   build decoder
-#
-g++ -I. -lxparam -g -pg -O3 -march=$1 -ffast-math -o debug_decoder decoder/decmain.cpp libdirac_decoder/*.cpp libdirac_common/*.cpp libdirac_motionest/*.cpp
-#
-# END
+
+make CXXFLAGS="-g -pg -O3 -ffast-math -march=$1"
