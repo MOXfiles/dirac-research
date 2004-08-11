1b6912528093da02c298fde555f8b42cdf2eabfb
asuraparaju <asuraparaju>
2004-08-11 14:53:26 +0000

    Added support for cut-detection and intra frame insertion. Changed output structure to output frame by frame, and added recoding to exploit this.  Recoding takes place if quality is not within a range of the target quality.
    Replaced tabs with spaces.
    
    Updated data file extension

--- encoder/encmain.cpp
+++ encoder/encmain.cpp
@@ -47,272 +47,297 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
 
 using namespace std;
 
 
 static void display_help()
 {
-	cout << "\nDIRAC wavelet video coder.";
-	cout << "\n";
-	cout << "\nUsage: progname -<flag1> [<flag_val>] ... <input1> <input2> ...";
-	cout << "\nIn case of multiple assignment to the same parameter, the last holds.";
-	cout << "\n";
-	cout << "\nName    Type   I/O Default Value Description";
-	cout << "\n====    ====   === ============= ===========                                       ";
-	cout << "\ninput   string  I  [ required ]  Input file name";
-	cout << "\noutput  string  I  [ required ]  Output file name";
-	cout << "\nstream  bool    I  true          Use streaming compression presets";
-	cout << "\nHD720p  bool    I  false         Use HD-720p compression presets";
-	cout << "\nHD1080  bool    I  false         Use HD-1080 compression presets";
-	cout << "\nSD576   bool    I  false         Use SD-576 compression presets";
-	cout << "\nL1_sep  ulong   I  0UL           Separation of L1 frames";
-	cout << "\nnum_L1  ulong   I  0UL           Number of L1 frames";
-	cout << "\nxblen   ulong   I  0UL           Overlapping block horizontal length";
-	cout << "\nyblen   ulong   I  0UL           Overlapping block vertical length";
-	cout << "\nxbsep   ulong   I  0UL           Overlapping block horizontal separation";
-	cout << "\nybsep   ulong   I  0UL           Overlapping block vertical separation";
-	cout << "\ncpd     ulong   I  0UL           Perceptual weighting - vertical cycles per degree";
-	cout << "\nqf      float   I  0.0F          Overall quality factor (0.0 - 10.0)";
-	cout << "\nverbose bool    I  false         Verbose mode";
-	cout << endl;
+    cout << "\nDIRAC wavelet video coder.";
+    cout << "\n";
+    cout << "\nUsage: progname -<flag1> [<flag_val>] ... <input1> <input2> ...";
+    cout << "\nIn case of multiple assignment to the same parameter, the last holds.";
+    cout << "\n";
+    cout << "\nName    Type   I/O Default Value Description";
+    cout << "\n====    ====   === ============= ===========                                       ";
+    cout << "\ninput   string  I  [ required ]  Input file name";
+    cout << "\noutput  string  I  [ required ]  Output file name";
+    cout << "\nstream  bool    I  true          Use streaming compression presets";
+    cout << "\nHD720p  bool    I  false         Use HD-720p compression presets";
+    cout << "\nHD1080  bool    I  false         Use HD-1080 compression presets";
+    cout << "\nSD576   bool    I  false         Use SD-576 compression presets";
+    cout << "\nstart   ulong   I  0UL           Frame number to start encoding from";
+    cout << "\nstop    ulong   I  EOF           Frame number after which encoding finishes";
+    cout << "\nL1_sep  ulong   I  0UL           Separation of L1 frames";
+    cout << "\nnum_L1  ulong   I  0UL           Number of L1 frames";
+    cout << "\nxblen   ulong   I  0UL           Overlapping block horizontal length";
+    cout << "\nyblen   ulong   I  0UL           Overlapping block vertical length";
+    cout << "\nxbsep   ulong   I  0UL           Overlapping block horizontal separation";
+    cout << "\nybsep   ulong   I  0UL           Overlapping block vertical separation";
+    cout << "\ncpd     ulong   I  0UL           Perceptual weighting - vertical cycles per degree";
+    cout << "\nqf      float   I  0.0F          Overall quality factor (0.0 - 10.0)";
+    cout << "\nverbose bool    I  false         Verbose mode";
+    cout << endl;
 }
 
 int main (int argc, char* argv[]){
-	/*********************************************************************************/
-			/**********  command line parameter parsing*********/
+    /*********************************************************************************/
+            /**********  command line parameter parsing*********/
 
-		 /********** create params object to handle command line parameter parsing*********/
-	//To do: put parsing in a different function/constructor.
+         /********** create params object to handle command line parameter parsing*********/
+    //To do: put parsing in a different function/constructor.
 
     // create a list of boolean options
-	set<string> bool_opts;
-	bool_opts.insert("verbose");
-	bool_opts.insert("stream");
-	bool_opts.insert("HD720p");
-	bool_opts.insert("HD1080");
-	bool_opts.insert("SD576");
+    set<string> bool_opts;
+    bool_opts.insert("verbose");
+    bool_opts.insert("stream");
+    bool_opts.insert("HD720p");
+    bool_opts.insert("HD1080");
+    bool_opts.insert("SD576");
 
-	CommandLine args(argc,argv,bool_opts);
+    CommandLine args(argc,argv,bool_opts);
 
-	//the variables we'll read parameters into
-	EncoderParams encparams;
-	OLBParams bparams(12, 12, 8, 8);
-	char input_name[84];//input name for pictures
-	char output_name[84];//output name for pictures
-	char bit_name[84];	//output name for the bitstream
-	string input,output;
+    //the variables we'll read parameters into
+    EncoderParams encparams;
 
-	//Set default values. To do: these should really be set in the constructor for the encoder parameters
-	//These default values assume a streaming preset.
+    OLBParams bparams(12, 12, 8, 8);
+    //input name for pictures
+    char input_name[84];
+    //output name for pictures
+    char output_name[84];
+    //output name for the bitstream
+    char bit_name[84];
+
+    string input,output;
+
+    // The start and end-points of the parts of the file to be coded
+    // (end_pos set to -1 means code to the end)
+    int start_pos = 0;
+    int end_pos = -1;
+
+    //Set default values. To do: these should really be set in the constructor for the encoder parameters
+    //These default values assume a streaming preset.
     encparams.SetQf(5.0);
-	encparams.SetL1Sep(3);
-	encparams.SetNumL1(11);
-	encparams.SetUFactor(3.0f);
-	encparams.SetVFactor(1.75f);
-	encparams.SetCPD(20.0f);
+    encparams.SetL1Sep(3);
+    encparams.SetNumL1(11);
+    encparams.SetUFactor(3.0f);
+    encparams.SetVFactor(1.75f);
+    encparams.SetCPD(20.0f);
 
-	encparams.SetVerbose( false );
+    encparams.SetVerbose( false );
 
-	if (argc<3)//need at least 3 arguments - the program name, an input and an output
-	{
-		display_help();
-	}
-	else//carry on!
-	{
-		// parse command-line arguments
-		//Do required inputs
-		if (args.GetInputs().size()==2){
-			input=args.GetInputs()[0];
-			output=args.GetInputs()[1];
-		}
+    if (argc<3)//need at least 3 arguments - the program name, an input and an output
+    {
+        display_help();
+    }
+    else//carry on!
+    {
+        // parse command-line arguments
+        //Do required inputs
+        if (args.GetInputs().size()==2){
+            input=args.GetInputs()[0];
+            output=args.GetInputs()[1];
+        }
 
-		//check we have real inputs
-		if ((input.length() == 0) || (output.length() ==0))
-		{
-			display_help();
-			exit(1);
-		}
+        //check we have real inputs
+        if ((input.length() == 0) || (output.length() ==0))
+        {
+            display_help();
+            exit(1);
+        }
 
-		for (size_t i=0;i<input.length();i++)
-			input_name[i]=input[i];
+        for (size_t i=0;i<input.length();i++)
+            input_name[i]=input[i];
 
-		input_name[input.length()] = '\0';
+        input_name[input.length()] = '\0';
 
-		for (size_t i=0;i<output.length();i++)
-			output_name[i]=output[i];
+        for (size_t i=0;i<output.length();i++)
+            output_name[i]=output[i];
 
-		output_name[output.length()] = '\0';
+        output_name[output.length()] = '\0';
 
-		strncpy(bit_name,output_name,84);
-		strcat(bit_name,".drc");
+        strncpy(bit_name,output_name,84);
+        strcat(bit_name,".drc");
 
         // attempt to create a directory for MvData
         char dir_path[120];
         strcpy(dir_path, output.c_str());
-        strcat(dir_path, "_mvdata");
+        strcat(dir_path, ".imt");
         encparams.SetOutputPath(dir_path);
         std::cerr<<std::endl<<"Creating file "<<dir_path;
         std::ofstream out(dir_path, std::ios::out);
         out.close();
 
-		//Now do the options
-		//Start with quantisation factors
-		for (vector<CommandLine::option>::const_iterator opt = args.GetOptions().begin();
-			opt != args.GetOptions().end(); ++opt){
-			if (opt->m_name == "qf")
-			{
-				encparams.SetQf( atof(opt->m_value.c_str()) );
-			}
-		}//opt
-
-
-		//Now do checking for presets
-		for (vector<CommandLine::option>::const_iterator opt = args.GetOptions().begin();
-			opt != args.GetOptions().end(); ++opt)
-		{
-			if (opt->m_name == "stream")
-			{
-				encparams.SetL1Sep(3);
-				encparams.SetNumL1(11);
-				bparams.SetXblen( 12 );
-				bparams.SetYblen( 12 );
-				bparams.SetXbsep( 8 );
-				bparams.SetXbsep( 8 );
-				encparams.SetUFactor(3.0f);
-				encparams.SetVFactor(1.75f);
-				encparams.SetCPD(20.0f);
-
-			}
-			else if (opt->m_name == "HD720p")
-			{
-				encparams.SetL1Sep(6);
-				encparams.SetNumL1(3);
-				bparams.SetXblen( 16 );
-				bparams.SetYblen( 16 );
-				bparams.SetXbsep( 10 );
-				bparams.SetXbsep( 12 );
-				encparams.SetUFactor(3.0f);
-				encparams.SetVFactor(1.75f);
-				encparams.SetCPD(20.0f);
+        //Now do the options
+        //Start with quantisation factors
+        for (vector<CommandLine::option>::const_iterator opt = args.GetOptions().begin();
+            opt != args.GetOptions().end(); ++opt){
+            if (opt->m_name == "qf")
+            {
+                encparams.SetQf( atof(opt->m_value.c_str()) );
             }
-			else if (opt->m_name == "HD1080")
-			{
-				encparams.SetL1Sep(3);
-				encparams.SetNumL1(3);
-				bparams.SetXblen( 20 );
-				bparams.SetYblen( 20 );
-				bparams.SetXbsep( 16 );
-				bparams.SetXbsep( 16 );
-				encparams.SetUFactor(3.0f);
-				encparams.SetVFactor(1.75f);
-				encparams.SetCPD(32.0f);
-			}
-			else if (opt->m_name == "SD576")
-			{
-				encparams.SetL1Sep(3);
-				encparams.SetNumL1(3);
+        }//opt
+
+
+        //Now do checking for presets
+        for (vector<CommandLine::option>::const_iterator opt = args.GetOptions().begin();
+            opt != args.GetOptions().end(); ++opt)
+        {
+            if (opt->m_name == "stream")
+            {
+                encparams.SetL1Sep(3);
+                encparams.SetNumL1(11);
                 bparams.SetXblen( 12 );
-				bparams.SetYblen( 12 );
-				bparams.SetXbsep( 8 );
-				bparams.SetXbsep( 8 );
-				encparams.SetUFactor(3.0f);
-				encparams.SetVFactor(1.75f);
-				encparams.SetCPD(32.0f);
-			}
-		}//opt
+                bparams.SetYblen( 12 );
+                bparams.SetXbsep( 8 );
+                bparams.SetXbsep( 8 );
+                encparams.SetUFactor(3.0f);
+                encparams.SetVFactor(1.75f);
+                encparams.SetCPD(20.0f);
 
-		//now go over again and override presets with other values
-		for (vector<CommandLine::option>::const_iterator opt = args.GetOptions().begin();
-			opt != args.GetOptions().end(); ++opt)
-		{
-			if (opt->m_name == "L1_sep")
-			{
-				encparams.SetL1Sep( strtoul(opt->m_value.c_str(),NULL,10) );
-			}
-			else if (opt->m_name == "num_L1")
-			{
-				encparams.SetNumL1( strtoul(opt->m_value.c_str(),NULL,10) );
-			}
-			else if (opt->m_name == "xblen")
-			{
-				bparams.SetXblen( strtoul(opt->m_value.c_str(),NULL,10) );
-			}
-			else if (opt->m_name == "yblen")
-			{
-				bparams.SetYblen( strtoul(opt->m_value.c_str(),NULL,10) );
-			}
-			else if (opt->m_name == "xbsep")
-			{
-				bparams.SetXbsep( strtoul(opt->m_value.c_str(),NULL,10) );
-			}
-			else if (opt->m_name == "ybsep")
-			{
-				bparams.SetYbsep( strtoul(opt->m_value.c_str(),NULL,10) );
-			}
-			else if (opt->m_name == "cpd")
-			{
-				encparams.SetCPD( strtoul(opt->m_value.c_str(),NULL,10) );
-			}
-			else if (opt->m_name == "verbose")
-			{
-				encparams.SetVerbose( true );
-			}
-		}//opt
+            }
+            else if (opt->m_name == "HD720p")
+            {
+                encparams.SetL1Sep(6);
+                encparams.SetNumL1(3);
+                bparams.SetXblen( 16 );
+                bparams.SetYblen( 16 );
+                bparams.SetXbsep( 10 );
+                bparams.SetXbsep( 12 );
+                encparams.SetUFactor(3.0f);
+                encparams.SetVFactor(1.75f);
+                encparams.SetCPD(20.0f);
+            }
+            else if (opt->m_name == "HD1080")
+            {
+                encparams.SetL1Sep(3);
+                encparams.SetNumL1(3);
+                bparams.SetXblen( 20 );
+                bparams.SetYblen( 20 );
+                bparams.SetXbsep( 16 );
+                bparams.SetXbsep( 16 );
+                encparams.SetUFactor(3.0f);
+                encparams.SetVFactor(1.75f);
+                encparams.SetCPD(32.0f);
+            }
+            else if (opt->m_name == "SD576")
+            {
+                encparams.SetL1Sep(3);
+                encparams.SetNumL1(3);
+                bparams.SetXblen( 12 );
+                bparams.SetYblen( 12 );
+                bparams.SetXbsep( 8 );
+                bparams.SetXbsep( 8 );
+                encparams.SetUFactor(3.0f);
+                encparams.SetVFactor(1.75f);
+                encparams.SetCPD(32.0f);
+            }
+        }//opt
 
-		//Now rationalise the GOP options
-		//this stuff should really be done in a constructor!
-		if (encparams.NumL1()<0){//don't have a proper GOP
-			encparams.SetL1Sep( std::max(1 , encparams.L1Sep()) );
-		}
-		else if (encparams.NumL1() == 0){//have I-frame only coding
-			encparams.SetL1Sep(0);
-		}
+        //now go over again and override presets with other values
+        for (vector<CommandLine::option>::const_iterator opt = args.GetOptions().begin();
+            opt != args.GetOptions().end(); ++opt)
+        {
+            if (opt->m_name == "L1_sep")
+            {
+                encparams.SetL1Sep( strtoul(opt->m_value.c_str(),NULL,10) );
+            }
+            else if (opt->m_name == "num_L1")
+            {
+                encparams.SetNumL1( strtoul(opt->m_value.c_str(),NULL,10) );
+            }
+            else if (opt->m_name == "xblen")
+            {
+                bparams.SetXblen( strtoul(opt->m_value.c_str(),NULL,10) );
+            }
+            else if (opt->m_name == "yblen")
+            {
+                bparams.SetYblen( strtoul(opt->m_value.c_str(),NULL,10) );
+            }
+            else if (opt->m_name == "xbsep")
+            {
+                bparams.SetXbsep( strtoul(opt->m_value.c_str(),NULL,10) );
+            }
+            else if (opt->m_name == "ybsep")
+            {
+                bparams.SetYbsep( strtoul(opt->m_value.c_str(),NULL,10) );
+            }
+            else if (opt->m_name == "cpd")
+            {
+                encparams.SetCPD( strtoul(opt->m_value.c_str(),NULL,10) );
+            }
+            else if (opt->m_name == "verbose")
+            {
+                encparams.SetVerbose( true );
+            }
+            else if (opt->m_name == "start")
+            {
+                start_pos = strtoul(opt->m_value.c_str(),NULL,10);
+            }
+            else if (opt->m_name == "stop")
+            {
+                end_pos = strtoul(opt->m_value.c_str(),NULL,10);
+            }
+        }//opt
+
+        //Now rationalise the GOP options
+        //this stuff should really be done in a constructor!
+        if (encparams.NumL1()<0){//don't have a proper GOP
+            encparams.SetL1Sep( std::max(1 , encparams.L1Sep()) );
+        }
+        else if (encparams.NumL1() == 0){//have I-frame only coding
+            encparams.SetL1Sep(0);
+        }
 
 
   /********************************************************************/
-	     //next do picture file stuff
+         //next do picture file stuff
 
-   		/* ------ open input files & get params -------- */
+           /* ------ open input files & get params -------- */
 
-		PicInput myinputpic(input_name);
-		myinputpic.ReadPicHeader();
+        PicInput myinputpic(input_name);
+        myinputpic.ReadPicHeader();
 
-  		/* ------- open output files and write the header -------- */
+          /* ------- open output files and write the header -------- */
 
-		PicOutput myoutputpic(output_name,myinputpic.GetSeqParams());
-		myoutputpic.WritePicHeader();
+        PicOutput myoutputpic(output_name,myinputpic.GetSeqParams());
+        myoutputpic.WritePicHeader();
 
-	/********************************************************************/
+    /********************************************************************/
 
 
-   		//set up all the block parameters so we have a self-consistent set
+           //set up all the block parameters so we have a self-consistent set
 
-		encparams.SetBlockSizes( bparams , myinputpic.GetSeqParams().CFormat() );
+        encparams.SetBlockSizes( bparams , myinputpic.GetSeqParams().CFormat() );
 
    /********************************************************************/
       //open the bitstream file
-		std::ofstream outfile(bit_name,std::ios::out | std::ios::binary);	//bitstream output
+        std::ofstream outfile(bit_name,std::ios::out | std::ios::binary);    //bitstream output
 
    /********************************************************************/
-    	//do the work!!
-		SequenceCompressor seq_compressor(&myinputpic,&outfile,encparams);
-		seq_compressor.CompressNextFrame();
-		for (int I=0 ; I<myinputpic.GetSeqParams().Zl() ; ++I)
-		{
-			if (!seq_compressor.Finished())
-				myoutputpic.WriteNextFrame( seq_compressor.CompressNextFrame() );
-		}//I
+        //do the work!!
+        SequenceCompressor seq_compressor(&myinputpic,&outfile,encparams);
+        seq_compressor.CompressNextFrame();
+
+        if ( end_pos == -1 )
+            end_pos = myinputpic.GetSeqParams().Zl()-1;
+
+        myinputpic.Skip( start_pos );
+
+        for (int z=start_pos ; z<=end_pos ; ++z)
+        {
+            if ( !seq_compressor.Finished() )
+                myoutputpic.WriteNextFrame( seq_compressor.CompressNextFrame() );
+        }//I
 
    /********************************************************************/
- 	//close the bitstream file
-		outfile.close();
+     //close the bitstream file
+        outfile.close();
 
-		if ( encparams.Verbose() )
-			std::cerr<<std::endl<<"Finished encoding\n";
-		return EXIT_SUCCESS;
+        if ( encparams.Verbose() )
+            std::cerr<<std::endl<<"Finished encoding\n";
+        return EXIT_SUCCESS;
 
 
-	}//?sufficient args?
+    }//?sufficient args?
 }
