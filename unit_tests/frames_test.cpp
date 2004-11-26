be016adfbcaf7d68534e42ceb7cccedeaed0250e
asuraparaju <asuraparaju>
2004-11-26 13:36:49 +0000

    Unit test for Motion Compensation

---
+++ unit_tests/frames_test.cpp
@@ -0,0 +1,263 @@
+/* ***** BEGIN LICENSE BLOCK *****
+*
+* $Id$ $Name$
+*
+* Version: MPL 1.1/GPL 2.0/LGPL 2.1
+*
+* The contents of this file are subject to the Mozilla Public License
+* Version 1.1 (the "License"); you may not use this file except in compliance
+* with the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+*
+* Software distributed under the License is distributed on an "AS IS" basis,
+* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+* the specific language governing rights and limitations under the License.
+*
+* The Original Code is Steve Bearcroft's code.
+*
+* The Initial Developer of the Original Code is Steve Bearcroft.
+* Portions created by the Initial Developer are Copyright (C) 2004.
+* All Rights Reserved.
+*
+* Contributor(s): Steve Bearcroft (Original Author)
+*
+* Alternatively, the contents of this file may be used under the terms of
+* the GNU General Public License Version 2 (the "GPL"), or the GNU Lesser
+* Public License Version 2.1 (the "LGPL"), in which case the provisions of
+* the GPL or the LGPL are applicable instead of those above. If you wish to
+* allow use of your version of this file only under the terms of the either
+* the GPL or LGPL and not to allow others to use your version of this file
+* under the MPL, indicate your decision by deleting the provisions above
+* and replace them with the notice and other provisions required by the GPL
+* or LGPL. If you do not delete the provisions above, a recipient may use
+* your version of this file under the terms of any one of the MPL, the GPL
+* or the LGPL.
+* ***** END LICENSE BLOCK ***** */
+#include "core_suite.h"
+#include "frames_test.h"
+#include "arrays_test.h"
+
+#include <libdirac_common/frame.h>
+using namespace dirac;
+
+#include <memory>
+
+//NOTE: ensure that the suite is added to the default registry in
+//cppunit_testsuite.cpp
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION (FramesTest, coreSuiteName());
+
+void FramesTest::setupFrame (Frame& frame, int start_val)
+{
+    setupPicArray(frame.Ydata(), start_val);
+    if (frame.GetFparams().CFormat() != Yonly)
+    {
+        setupPicArray(frame.Udata(), start_val);
+        setupPicArray(frame.Vdata(), start_val);
+    }
+}
+
+bool FramesTest::setupPicArray (PicArray &arr, int start_val)
+{
+    char value =start_val; // use char to limit values to 8 bits
+    int err_count = 0;
+    int i, j;
+    for ( i =arr.FirstY(); i <= arr.LastY(); i++)
+    {
+        for ( j =arr.FirstX(); j <= arr.LastX(); j++)
+        {
+            arr[i][j] = ++value;
+        }
+    }
+    value = start_val;
+    for ( i =arr.FirstY(); i <= arr.LastY(); i++)
+    {
+        for ( j =arr.FirstX(); j <= arr.LastX(); j++)
+        {
+            ++value;
+            if (arr[i][j] != value)
+                err_count++;
+        }
+    }
+    CPPUNIT_ASSERT_EQUAL (err_count, 0);
+    return true;
+}
+
+void FramesTest::zeroFrame (Frame& frame)
+{
+    zeroPicArray(frame.Ydata());
+    if (frame.GetFparams().CFormat() != Yonly)
+    {
+        zeroPicArray(frame.Udata());
+        zeroPicArray(frame.Vdata());
+    }
+}
+
+bool FramesTest::zeroPicArray (PicArray &arr)
+{
+    short value =0;
+    int err_count = 0;
+    int i, j;
+    for ( i =arr.FirstY(); i <= arr.LastY(); i++)
+    {
+        for ( j =arr.FirstX(); j <= arr.LastX(); j++)
+        {
+            arr[i][j] = value;
+        }
+    }
+    value = 0;
+    for ( i =arr.FirstY(); i <= arr.LastY(); i++)
+    {
+        for ( j =arr.FirstX(); j <= arr.LastX(); j++)
+        {
+            if (arr[i][j] != value)
+                err_count++;
+        }
+    }
+    CPPUNIT_ASSERT_EQUAL (err_count, 0);
+    return true;
+}
+
+
+bool FramesTest::equalPicArrays (const PicArray &lhs, const PicArray &rhs)
+{
+    CPPUNIT_ASSERT_EQUAL (lhs.CSort(), rhs.CSort());
+    CPPUNIT_ASSERT_EQUAL (lhs.LengthX(), rhs.LengthX());
+    CPPUNIT_ASSERT_EQUAL (lhs.LengthY(), rhs.LengthY());
+    CPPUNIT_ASSERT_EQUAL (lhs.FirstX(), rhs.FirstX());
+    CPPUNIT_ASSERT_EQUAL (lhs.FirstY(), rhs.FirstY());
+    CPPUNIT_ASSERT_EQUAL (lhs.LastX(), rhs.LastX() );
+    CPPUNIT_ASSERT_EQUAL (lhs.LastY(), rhs.LastY() );
+
+    for (int i =lhs.FirstY(); i <= lhs.LastY(); i++)
+    {
+        ValueType * lshRow = lhs[i];
+        ValueType * rshRow = rhs[i];
+        for (int j =lhs.FirstX(); j <= lhs.LastX(); j++)
+        {
+            if (!( lshRow[j] == rshRow[j]))
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+
+bool FramesTest::almostEqualPicArrays (const PicArray &lhs, const PicArray &rhs, int allowedError)
+{
+    CPPUNIT_ASSERT_EQUAL (lhs.CSort(), rhs.CSort());
+    CPPUNIT_ASSERT_EQUAL (lhs.LengthX(), rhs.LengthX());
+    CPPUNIT_ASSERT_EQUAL (lhs.LengthY(), rhs.LengthY());
+    CPPUNIT_ASSERT_EQUAL (lhs.FirstX(), rhs.FirstX());
+    CPPUNIT_ASSERT_EQUAL (lhs.FirstY(), rhs.FirstY());
+    CPPUNIT_ASSERT_EQUAL (lhs.LastX(), rhs.LastX() );
+    CPPUNIT_ASSERT_EQUAL (lhs.LastY(), rhs.LastY() );
+
+    for (int i =lhs.FirstY(); i <= lhs.LastY(); i++)
+    {
+        ValueType * lshRow = lhs[i];
+        ValueType * rshRow = rhs[i];
+        for (int j =lhs.FirstX(); j <= lhs.LastX(); j++)
+        {
+            if ( allowedError < std::abs(lshRow[j] - rshRow[j]))
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool FramesTest::equalFrames (const Frame &lhs, const Frame &rhs)
+{
+    CPPUNIT_ASSERT_EQUAL (lhs.GetFparams().CFormat(), rhs.GetFparams().CFormat() );
+    CPPUNIT_ASSERT (equalPicArrays(lhs.Ydata(), rhs.Ydata()));
+    if (lhs.GetFparams().CFormat() != Yonly)
+    {
+        CPPUNIT_ASSERT (equalPicArrays(lhs.Udata(), rhs.Udata()));
+        CPPUNIT_ASSERT (equalPicArrays(lhs.Vdata(), rhs.Vdata()));
+    }
+
+    return true;
+}
+
+bool FramesTest::almostEqualFrames (const Frame &lhs, const Frame &rhs, int allowedError)
+{
+    CPPUNIT_ASSERT_EQUAL (lhs.GetFparams().CFormat(), rhs.GetFparams().CFormat() );
+    CPPUNIT_ASSERT (almostEqualPicArrays(lhs.Ydata(), rhs.Ydata(), allowedError));
+    if (lhs.GetFparams().CFormat() != Yonly)
+    {
+        CPPUNIT_ASSERT (almostEqualPicArrays(lhs.Udata(), rhs.Udata(), allowedError));
+        CPPUNIT_ASSERT (almostEqualPicArrays(lhs.Vdata(), rhs.Vdata(), allowedError));
+    }
+
+    return true;
+}
+
+FramesTest::FramesTest()
+{
+}
+
+FramesTest::~FramesTest()
+{
+}
+
+void FramesTest::setUp()
+{
+}
+
+void FramesTest::tearDown()
+{
+}
+
+void FramesTest::testConstructor()
+{
+    FrameParams f_params(Yonly, 20, 30);
+    Frame frame(f_params);
+
+    CPPUNIT_ASSERT_EQUAL (20, frame.Ydata().LengthX());
+    CPPUNIT_ASSERT_EQUAL (30, frame.Ydata().LengthY());
+    CPPUNIT_ASSERT_EQUAL (20, frame.Ydata().LastX() - frame.Ydata().FirstX() + 1);
+    CPPUNIT_ASSERT_EQUAL (30, frame.Ydata().LastY() - frame.Ydata().FirstY() + 1);
+}
+
+void FramesTest::testDefaultFParam()
+{
+    FrameParams f_params;
+    Frame frame(f_params);
+
+    CPPUNIT_ASSERT_EQUAL (0, frame.Ydata().LengthX());
+    CPPUNIT_ASSERT_EQUAL (0, frame.Ydata().LengthY());
+    CPPUNIT_ASSERT_EQUAL (0, frame.Ydata().FirstX());
+    CPPUNIT_ASSERT_EQUAL (0, frame.Ydata().FirstY());
+    CPPUNIT_ASSERT_EQUAL (-1, frame.Ydata().LastX());
+    CPPUNIT_ASSERT_EQUAL (-1, frame.Ydata().LastY());
+}
+
+void FramesTest::testCopyConstructor()
+{
+    FrameParams f_params(Yonly, 20, 30);
+    Frame frame(f_params);
+    setupFrame(frame, 0);
+    
+    Frame frame_copy(frame);
+    CPPUNIT_ASSERT (equalFrames (frame, frame_copy));
+}
+
+void FramesTest::testAssignment()
+{
+    FrameParams f_params(Yonly, 20, 30);
+    Frame frame(f_params);
+    setupFrame(frame, 0);
+
+    FrameParams f_params_copy(Yonly,10,10);
+    Frame frame_copy(f_params_copy);
+
+    frame_copy = frame;
+
+    CPPUNIT_ASSERT (equalFrames (frame, frame_copy));
+}
+
---
+++ unit_tests/motion_comp_test.cpp
@@ -0,0 +1,380 @@
+/* ***** BEGIN LICENSE BLOCK *****
+*
+* $Id$ $Name$
+*
+* Version: MPL 1.1/GPL 2.0/LGPL 2.1
+*
+* The contents of this file are subject to the Mozilla Public License
+* Version 1.1 (the "License"); you may not use this file except in compliance
+* with the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+*
+* Software distributed under the License is distributed on an "AS IS" basis,
+* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+* the specific language governing rights and limitations under the License.
+*
+* The Original Code is Steve Bearcroft's code.
+*
+* The Initial Developer of the Original Code is Steve Bearcroft.
+* Portions created by the Initial Developer are Copyright (C) 2004.
+* All Rights Reserved.
+*
+* Contributor(s): Steve Bearcroft (Original Author)
+*                 Anuradha Suraparaju
+*
+* Alternatively, the contents of this file may be used under the terms of
+* the GNU General Public License Version 2 (the "GPL"), or the GNU Lesser
+* Public License Version 2.1 (the "LGPL"), in which case the provisions of
+* the GPL or the LGPL are applicable instead of those above. If you wish to
+* allow use of your version of this file only under the terms of the either
+* the GPL or LGPL and not to allow others to use your version of this file
+* under the MPL, indicate your decision by deleting the provisions above
+* and replace them with the notice and other provisions required by the GPL
+* or LGPL. If you do not delete the provisions above, a recipient may use
+* your version of this file under the terms of any one of the MPL, the GPL
+* or the LGPL.
+* ***** END LICENSE BLOCK ***** */
+#include "core_suite.h"
+#include "motion_comp_test.h"
+#include "frames_test.h"
+
+#include <libdirac_common/frame.h>
+#include <libdirac_common/frame_buffer.h>
+#include <libdirac_common/mot_comp.h>
+using namespace dirac;
+
+#include <memory>
+
+//NOTE: ensure that the suite is added to the default registry in
+//cppunit_testsuite.cpp
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION (MotionCompTest, coreSuiteName());
+
+#define X_SIZE  352
+#define Y_SIZE  288
+
+MotionCompTest::MotionCompTest()
+{
+}
+
+MotionCompTest::~MotionCompTest()
+{
+}
+
+void MotionCompTest::setUp()
+{
+}
+
+void MotionCompTest::tearDown()
+{
+}
+
+
+MvData* setupMV1Data(CodecParams& cp, int mv_x, int mv_y, PredMode mode)
+{
+    MvData* mv_data = new MvData(cp.XNumMB(),cp.YNumMB(),cp.XNumBlocks(),cp.YNumBlocks());
+    MvArray& arr = mv_data->Vectors(1);
+    for (int i =arr.FirstY(); i <= arr.LastY(); i++)
+    {
+        for (int j =arr.FirstX(); j <= arr.LastX(); j++)
+        {
+            arr[i][j].x = mv_x;
+            arr[i][j].y = mv_y;
+            mv_data->Mode()[i][j] = mode;
+        }
+    }
+    return mv_data;
+}
+
+void setupMV2Data(MvData* mv_data, int mv_x, int mv_y)
+{
+    MvArray& arr = mv_data->Vectors(2);
+    for (int i =arr.FirstY(); i <= arr.LastY(); i++)
+    {
+        for (int j =arr.FirstX(); j <= arr.LastX(); j++)
+        {
+            arr[i][j].x = mv_x;
+            arr[i][j].y = mv_y;
+        }
+    }
+}
+
+void MotionCompTest::testZeroMotionComp()
+{
+    FrameBuffer fbuffer(format420, X_SIZE, Y_SIZE);
+    CodecParams cp;
+    OLBParams bparams(12, 12, 8, 8);
+    cp.SetBlockSizes(bparams, fbuffer.GetFParams().CFormat());
+    cp.SetXNumMB( X_SIZE / cp.LumaBParams(0).Xbsep() );
+    cp.SetYNumMB( Y_SIZE / cp.LumaBParams(0).Ybsep() );
+
+    cp.SetXNumBlocks( 4*cp.XNumMB() );
+    cp.SetYNumBlocks( 4*cp.YNumMB() );
+
+
+    MotionCompensator mc( cp, ADD );
+    
+    MvData* mv_data = setupMV1Data(cp, 0, 0, REF1_ONLY);
+
+    FrameParams fp(format420, X_SIZE, Y_SIZE);
+
+    fp.SetFSort(I_frame);
+    fp.SetFrameNum(0);
+    fbuffer.PushFrame(fp);
+    FramesTest::setupFrame(fbuffer.GetFrame(0),0);
+
+    fp.SetFSort(L1_frame);
+    fp.SetFrameNum(1);
+    fp.Refs().push_back(0);
+    fbuffer.PushFrame(fp);
+    FramesTest::zeroFrame(fbuffer.GetFrame(1));
+
+    fp.SetFrameNum(2);
+    fbuffer.PushFrame(fp);
+    FramesTest::zeroFrame(fbuffer.GetFrame(2));
+
+    mc.CompensateFrame(fbuffer, 1, *mv_data);
+
+    MotionCompensator mc2( cp, SUBTRACT );
+
+    //too many rounding errors for this to be exactly true;
+    CPPUNIT_ASSERT (FramesTest::almostEqualFrames (fbuffer.GetFrame(0), fbuffer.GetFrame(1), 2));
+    mc2.CompensateFrame(fbuffer, 1, *mv_data);
+
+    CPPUNIT_ASSERT (FramesTest::equalFrames (fbuffer.GetFrame(2), fbuffer.GetFrame(1)));
+    delete mv_data;
+}
+void MotionCompTest::testAddandSubMotionComp()
+{
+    FrameBuffer fbuffer(format420, X_SIZE, Y_SIZE);
+    CodecParams cp;
+    OLBParams bparams(12, 12, 8, 8);
+    cp.SetBlockSizes(bparams, fbuffer.GetFParams().CFormat());
+    cp.SetXNumMB( X_SIZE / cp.LumaBParams(0).Xbsep() );
+    cp.SetYNumMB( Y_SIZE / cp.LumaBParams(0).Ybsep() );
+
+    cp.SetXNumBlocks( 4*cp.XNumMB() );
+    cp.SetYNumBlocks( 4*cp.YNumMB() );
+
+    
+    MvData* mv_data = setupMV1Data(cp, 5, 5, REF1_ONLY);
+
+    FrameParams fp(format420, X_SIZE, Y_SIZE);
+
+    fp.SetFSort(I_frame);
+    fp.SetFrameNum(0);
+    fbuffer.PushFrame(fp);
+    FramesTest::setupFrame(fbuffer.GetFrame(0),0);
+
+    fp.SetFSort(L1_frame);
+    fp.SetFrameNum(1);
+    fp.Refs().push_back(0);
+    fbuffer.PushFrame(fp);
+    FramesTest::zeroFrame(fbuffer.GetFrame(1));
+
+    fp.SetFrameNum(2);
+    fbuffer.PushFrame(fp);
+    FramesTest::zeroFrame(fbuffer.GetFrame(2));
+
+    MotionCompensator mc( cp, ADD );
+    mc.CompensateFrame(fbuffer, 1, *mv_data);
+
+    MotionCompensator mc2( cp, SUBTRACT );
+    mc2.CompensateFrame(fbuffer, 1, *mv_data);
+
+    CPPUNIT_ASSERT (FramesTest::equalFrames (fbuffer.GetFrame(2), fbuffer.GetFrame(1)));
+    delete mv_data;
+}
+
+void MotionCompTest::testL2_frame()
+{
+    FrameBuffer fbuffer(format420, X_SIZE, Y_SIZE);
+    CodecParams cp;
+    OLBParams bparams(12, 12, 8, 8);
+    cp.SetBlockSizes(bparams, fbuffer.GetFParams().CFormat());
+    cp.SetXNumMB( X_SIZE / cp.LumaBParams(0).Xbsep() );
+    cp.SetYNumMB( Y_SIZE / cp.LumaBParams(0).Ybsep() );
+
+    cp.SetXNumBlocks( 4*cp.XNumMB() );
+    cp.SetYNumBlocks( 4*cp.YNumMB() );
+
+    
+    MvData* mv_data = setupMV1Data(cp, 5, 5, REF1_ONLY);
+
+    FrameParams fp(format420, X_SIZE, Y_SIZE);
+
+    fp.SetFSort(I_frame);
+    fp.SetFrameNum(0);
+    fbuffer.PushFrame(fp);
+    FramesTest::setupFrame(fbuffer.GetFrame(0),0);
+
+    fp.SetFSort(L2_frame);
+    fp.SetFrameNum(1);
+    fp.Refs().push_back(0);
+    fbuffer.PushFrame(fp);
+    FramesTest::zeroFrame(fbuffer.GetFrame(1));
+
+    fp.SetFrameNum(2);
+    fbuffer.PushFrame(fp);
+    FramesTest::zeroFrame(fbuffer.GetFrame(2));
+
+    MotionCompensator mc( cp, ADD );
+    mc.CompensateFrame(fbuffer, 1, *mv_data);
+
+    MotionCompensator mc2( cp, SUBTRACT );
+    mc2.CompensateFrame(fbuffer, 1, *mv_data);
+
+    CPPUNIT_ASSERT (FramesTest::equalFrames (fbuffer.GetFrame(2), fbuffer.GetFrame(1)));
+    delete mv_data;
+}
+
+
+void MotionCompTest::testI_frame()
+{
+    FrameBuffer fbuffer(format420, X_SIZE, Y_SIZE);
+    CodecParams cp;
+    OLBParams bparams(12, 12, 8, 8);
+    cp.SetBlockSizes(bparams, fbuffer.GetFParams().CFormat());
+    cp.SetXNumMB( X_SIZE / cp.LumaBParams(0).Xbsep() );
+    cp.SetYNumMB( Y_SIZE / cp.LumaBParams(0).Ybsep() );
+
+    cp.SetXNumBlocks( 4*cp.XNumMB() );
+    cp.SetYNumBlocks( 4*cp.YNumMB() );
+
+
+    
+    MvData* mv_data = setupMV1Data(cp, 5, 5, REF1_ONLY);
+
+    FrameParams fp(format420, X_SIZE, Y_SIZE);
+
+    fp.SetFSort(I_frame);
+    fp.SetFrameNum(0);
+    fbuffer.PushFrame(fp);
+    FramesTest::setupFrame(fbuffer.GetFrame(0),0);
+
+    fp.SetFSort(I_frame);
+    fp.SetFrameNum(1);
+    fp.Refs().push_back(0);
+    fbuffer.PushFrame(fp);
+    FramesTest::setupFrame(fbuffer.GetFrame(1),0);
+
+
+    MotionCompensator mc( cp, ADD );
+    mc.CompensateFrame(fbuffer, 1, *mv_data);
+
+    CPPUNIT_ASSERT (FramesTest::equalFrames (fbuffer.GetFrame(0), fbuffer.GetFrame(1)));
+    delete mv_data;
+}
+
+
+void MotionCompTest::testRef2()
+{
+    FrameBuffer fbuffer(format420, X_SIZE, Y_SIZE);
+    CodecParams cp;
+    OLBParams bparams(12, 12, 8, 8);
+    cp.SetBlockSizes(bparams, fbuffer.GetFParams().CFormat());
+    cp.SetXNumMB( X_SIZE / cp.LumaBParams(0).Xbsep() );
+    cp.SetYNumMB( Y_SIZE / cp.LumaBParams(0).Ybsep() );
+
+    cp.SetXNumBlocks( 4*cp.XNumMB() );
+    cp.SetYNumBlocks( 4*cp.YNumMB() );
+
+
+    
+    MvData* mv_data = setupMV1Data(cp, 5, 5, REF2_ONLY);
+    setupMV2Data(mv_data, 0, 0);
+
+    FrameParams fp(format420, X_SIZE, Y_SIZE);
+
+    fp.SetFSort(I_frame);
+    fp.SetFrameNum(0);
+    fbuffer.PushFrame(fp);
+    FramesTest::setupFrame(fbuffer.GetFrame(0),0);
+
+    fp.SetFSort(L1_frame);
+    fp.SetFrameNum(1);
+    fp.Refs().push_back(2);
+    fp.Refs().push_back(0);
+    fbuffer.PushFrame(fp);
+    FramesTest::zeroFrame(fbuffer.GetFrame(1));
+
+    fp.SetFrameNum(2);
+    fbuffer.PushFrame(fp);
+    FramesTest::zeroFrame(fbuffer.GetFrame(2));
+
+    MotionCompensator mc( cp, ADD );
+    mc.CompensateFrame(fbuffer, 1, *mv_data);
+
+    CPPUNIT_ASSERT (FramesTest::almostEqualFrames (fbuffer.GetFrame(0), fbuffer.GetFrame(1), 2));
+
+    MotionCompensator mc2( cp, SUBTRACT );
+    mc2.CompensateFrame(fbuffer, 1, *mv_data);
+
+    CPPUNIT_ASSERT (FramesTest::equalFrames (fbuffer.GetFrame(2), fbuffer.GetFrame(1)));
+    delete mv_data;
+}
+
+void MotionCompTest::testRef1and2()
+{
+    FrameBuffer fbuffer(format420, X_SIZE, Y_SIZE);
+    CodecParams cp;
+    OLBParams bparams(12, 12, 8, 8);
+    cp.SetBlockSizes(bparams, fbuffer.GetFParams().CFormat());
+    cp.SetXNumMB( X_SIZE / cp.LumaBParams(0).Xbsep() );
+    cp.SetYNumMB( Y_SIZE / cp.LumaBParams(0).Ybsep() );
+
+    cp.SetXNumBlocks( 4*cp.XNumMB() );
+    cp.SetYNumBlocks( 4*cp.YNumMB() );
+    
+    MvData* mv_data = setupMV1Data(cp, 5, 5, REF1_ONLY);
+    setupMV2Data(mv_data, 5, 5);
+
+    MvData* mv_data1 = setupMV1Data(cp, 7, 3, REF2_ONLY);
+    setupMV2Data(mv_data1, 7, 3);
+
+    MvData* mv_data2 = setupMV1Data(cp, 5, 5, REF1AND2);
+    setupMV2Data(mv_data2, 7, 3);
+
+    FrameParams fp(format420, X_SIZE, Y_SIZE);
+
+    fp.SetFSort(I_frame);
+    fp.SetFrameNum(0);
+    fbuffer.PushFrame(fp);
+    FramesTest::setupFrame(fbuffer.GetFrame(0),0);
+
+    fp.SetFSort(I_frame);
+    fp.SetFrameNum(1);
+    fbuffer.PushFrame(fp);
+    FramesTest::setupFrame(fbuffer.GetFrame(1),50);
+
+    fp.SetFSort(L1_frame);
+    fp.SetFrameNum(2);
+    fp.Refs().push_back(0);
+    fp.Refs().push_back(1);
+    fbuffer.PushFrame(fp);
+    FramesTest::zeroFrame(fbuffer.GetFrame(2));
+
+    fp.SetFrameNum(3);
+    fbuffer.PushFrame(fp);
+    FramesTest::zeroFrame(fbuffer.GetFrame(3));
+
+    MotionCompensator mc( cp, ADD );
+
+    mc.CompensateFrame(fbuffer, 2, *mv_data);
+
+    MotionCompensator mc2( cp, ADD );
+
+    mc2.CompensateFrame(fbuffer, 2, *mv_data1);
+
+    MotionCompensator mc3( cp, ADD );
+
+    mc3.CompensateFrame(fbuffer, 3, *mv_data2);
+
+    MotionCompensator mc4( cp, ADD );
+
+    mc4.CompensateFrame(fbuffer, 3, *mv_data2);
+
+    CPPUNIT_ASSERT (FramesTest::almostEqualFrames (fbuffer.GetFrame(2), fbuffer.GetFrame(3), 5    ));
+    delete mv_data;
+    delete mv_data1;
+    delete mv_data2;
+}
