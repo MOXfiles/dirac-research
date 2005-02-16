f15e421a20299902f1e69ff490f03612cd3c06e4
asuraparaju <asuraparaju>
2005-02-16 17:52:05 +0000

    the bitstream has changed. So updated bitstream version so that an error is reported if attempting to decode  older versions of Dirac files.

--- libdirac_common/bit_manager.h
+++ libdirac_common/bit_manager.h
@@ -72,7 +72,7 @@ namespace dirac
     //! Not a start code but part of data
     const unsigned char NOT_START_CODE = 0xFF;
     //! Bitstream version
-    const unsigned char BITSTREAM_VERSION = 0x02;  //0.2
+    const unsigned char BITSTREAM_VERSION = 0x03;  //0.3
 
 
     ////////////////////////////////////////////////
