206d1c88106ca41c64528fcc2b4abf508c087c0c
asuraparaju <asuraparaju>
2005-05-20 12:44:59 +0000

    Remove all references to make_header. Sequence header information is now passed to encoder on the command line.

--- tests/create_dirac_testfile.pl
+++ tests/create_dirac_testfile.pl
@@ -9,12 +9,9 @@ my %args;
 
 GetOptions (\%args,
             'convutildir=s', #Directory which contains dirac conversion utilities
-            'hdrutildir=s', #Directory which contains dirac picheader utilities
-			'onlyhdrs', #create only headers
 			'width=i', #input frame width
 			'height=i', #input frame height
 			'num_frames=i', #number of input frames to use
-			'frame_rate=s', #frames per sec
 			'use', #Print usage information
 			);
 
@@ -26,7 +23,6 @@ my $num_files = scalar @ARGV;
 my @chroma = qw (411 420 422 444);
 
 $args{convutildir} .= "/" if (defined $args{convutildir});
-$args{hdrutildir} .= "/" if (defined $args{hdrutildir});
 
 for ( my $i = 0; $i < scalar(@chroma) ; ++$i)
 {
@@ -34,8 +30,6 @@ for ( my $i = 0; $i < scalar(@chroma) ; ++$i)
 	system ("$conv > /dev/null 2>&1") == 0 || die "$conv not found";
 }
 
-my $hdrutil =  $args{hdrutildir}."make_header";
-system ("$hdrutil > /dev/null 2>&1") == 0 || die "$hdrutil not found";
 
 for (my $i = 0; $i < $num_files; $i++)
 {
@@ -48,27 +42,7 @@ for (my $i = 0; $i < $num_files; $i++)
 	$height = $args{height} if defined $args{height};
 	$num_frames = $args{num_frames} if defined $args{num_frames};
 
-    my $frame_rate_num = 25;
-    my $frame_rate_denom = 1;
 
-	if ($height == 288)
-	{ $frame_rate_num = 13; }
-	elsif ($height == 720)
-	{ $frame_rate_num = 50; }
-
-	if (defined $args{frame_rate})
-	{
-		if ($args{frame_rate} =~ /^\d+$/)
-		{ $frame_rate_num =  $args{frame_rate}; }
-		elsif ($args{frame_rate} =~ /^(\d+)\/(\d+)$/)
-		{ $frame_rate_num = $1; $frame_rate_denom = $2; }
-		else
-		{ 
-			print "Incorrect frame rate format ($args{frame_rate}). Ignoring...\n";
-		}
-	}
-	#my $frame_rate = $frame_rate_num . "/" . $frame_rate_denom;
-	my $frame_rate = $frame_rate_num;
 	my $yuv_file = basename($ARGV[$i]);
 	$yuv_file =~ s/\.rgb//g;
 	print "\nConverting $ARGV[$i] - ";
@@ -83,10 +57,6 @@ for (my $i = 0; $i < $num_files; $i++)
 			my $conv =$args{convutildir}. "RGBtoYUV" . $chroma[$j];
 			system("$conv $width $height $num_frames < $ARGV[$i] > $yuv.yuv 2>/dev/null") == 0 || die "$conv failed: $!";
 		}
-		print "\tCreating $chroma[$j] hdr file\n";
-		my $cformat = "format" . $chroma[$j];
-		my $hdr = $args{hdrutildir} . "make_header";
-		system("$hdr -xl $width -yl $height -framerate $frame_rate -cformat $cformat $yuv") == 0 || die "make_header failed: $!";
 	}
 
 	#now create the yonly file
@@ -108,9 +78,6 @@ for (my $i = 0; $i < $num_files; $i++)
 		close(IN);
 		close(OUT);
 	}
-	print "\tCreating Yonly hdr file\n";
-	my $hdr = $args{hdrutildir} . "make_header";
-	system("$hdr -xl $width -yl $height -framerate $frame_rate -cformat Yonly $yonly") == 0 || die "make_header failed";
 }
 
 sub printUsage
@@ -128,20 +95,14 @@ sub printUsage
 	print "                       utilities are available. If not specified, it looks\n";
 	print "                       for the utilities in the directories in the PATH \n";
 	print "                       variable.\n" ;
-	print "-hdrutildir=dir-name   dir-name is the directory where the Dirac picheader\n";
-	print "                       utilities are available. If not specified, it looks\n";
-	print "                       for the utilities in the directories in the PATH \n";
-	print "                       variable.\n" ;
-	print "-onlyhdrs              only create the image header files\n" ;
 	print "-width=w               w is the width of input frame\n" ;
 	print "-height=h              h is the height of input frame\n" ;
 	print "-num_frames=n          n is the number of input frames to use\n" ;
-	print "-frame_rate=r          r is the input frames per sec. It can be specified\n";
 	print "                       as an integer (25), or a rational number (30000/1001)\n";
 	print "                       If not specified a default is used based on the frame\n";
 	print "                       width\n";
 	print "-use                   Print usage info and exit\n";
 	print "\nExample:\n";
-	print "\t create_dirac_testfile.pl -convutildir=/home/guest/dirac-0.5.0/util/conversion -hdrutildir=/home/guest/dirac-0.5.0/picheader -frame_rate=13 /home/guest/rgb/snowboard-jum-352x288x75.rgb /home/guest/rgb/squirrel-352x288x75.rgb\n";
+	print "\t create_dirac_testfile.pl -convutildir=/home/guest/dirac-0.5.0/util/conversion /home/guest/rgb/snowboard-jum-352x288x75.rgb /home/guest/rgb/squirrel-352x288x75.rgb\n";
 	exit 0;
 }
