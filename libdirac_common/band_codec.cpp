b112240ad4ce1877d4f36055d7f66baf7262f7ff
asuraparaju <asuraparaju>
2005-12-02 10:16:52 +0000

    Added Steve Bearcroft to list of contributors.

--- libdirac_common/band_codec.cpp
+++ libdirac_common/band_codec.cpp
@@ -20,7 +20,9 @@
 * Portions created by the Initial Developer are Copyright (c) 2004.
 * All Rights Reserved.
 *
-* Contributor(s): Thomas Davies (Original Author), Scott R Ladd
+* Contributor(s): Thomas Davies (Original Author),
+*                 Scott R Ladd,
+*                 Steve Bearcroft
 *
 * Alternatively, the contents of this file may be used under the terms of
 * the GNU General Public License Version 2 (the "GPL"), or the GNU Lesser
